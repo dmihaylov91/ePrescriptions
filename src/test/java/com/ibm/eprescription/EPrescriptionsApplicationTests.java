7443a6efda92400d738fd810ad1d63b1205d26ce
Dimitar Mihaylov <dimitar.mihaylov@bg.ibm.com>
2020-11-25 10:39:00 +0200

    Initial commit

---
+++ src/test/java/com/ibm/eprescription/EPrescriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.eprescription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPrescriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
